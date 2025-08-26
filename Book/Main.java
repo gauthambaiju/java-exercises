aab4a7345f164b51d832c9636f24eff37af88922
gauthambaiju <gauthambaiju040@gmail.com>
2025-08-26 19:10:36 +0530

    feat: Added Book class

---
+++ Book/Main.java
@@ -0,0 +1,8 @@
+package Book;
+
+public class Main {
+    public static void main(String[] args) {
+        Book book = new Book("Book1", "Author1", 123.00);
+        System.out.println(book);
+    }
+}
