5ee2decf563539432e1473e2891c8f8eee445829
gauthambaiju <gauthambaiju040@gmail.com>
2025-08-29 09:30:06 +0530

    feat: Add class Student with name, age and grade

---
+++ Student_4/Main.java
@@ -0,0 +1,10 @@
+package Student_4;
+
+public class Main {
+    public static void main(String[] args) {
+        Student athira = new Student("Athira", 24, 4.0);
+        Student neha = new Student("Neha", 23, 4.5);
+        System.out.println(athira);
+        System.out.println(neha);
+    }
+}
