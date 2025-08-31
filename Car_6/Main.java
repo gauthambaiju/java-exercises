85b2dd40116fc6673b99e5267ea66b950321110a
gauthambaiju <gauthambaiju040@gmail.com>
2025-08-31 22:53:24 +0530

    feat: Add class Car to display info

---
+++ Car_6/Main.java
@@ -0,0 +1,9 @@
+package Car_6;
+
+public class Main {
+    public static void main(String[] args) {
+        Car car = new Car("BMW", "hehe", 1969);
+        System.out.println(car);
+        car.displayInfo();
+    }
+}
